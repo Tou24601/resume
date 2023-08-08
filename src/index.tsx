25fdff10f9496adcb2e43d025f75cc8fb70a9d53
Joanna Kolakowska <kolakowska.joanna.jk@gmail.com>
2023-08-08 22:07:08 +0200

    started redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import store from './store'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,7 +12,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
