2ed016525d96a4726a02932b5274fd87108a1592
joannakolakowska <kolakowska.joanna.jk@gmail.com>
2023-11-22 20:27:30 +0100

    add publications and build

--- src/pages/Publications.tsx
+++ src/pages/Publications.tsx
@@ -9,22 +9,29 @@ const Publications = ({ language }: Props) => {
     let data = require(`../data/publications-${language}.json`);
 
     interface Publication {
-        title: string,
-        publishingCompany: string,
-        year: string | number,
-        city: string,
-        authors: string
-    } 
-    return (
-      <ul className="listStyleNone">
-        {data.publications.map((item: Publication) => (
-          <li key={data.publications.indexOf(item)}>
-            <h4 className="fw-bold">{item.title}</h4>
-            <p>{item.authors}, {item.publishingCompany}, {item.city}&nbsp;{item.year}</p>
-          </li>
-        ))}
-      </ul>
-    );
+      title: string;
+      publishingCompany: string;
+      year: string | number;
+      city: string;
+      authors: string;
+      link?: string;
+    }
+
+      return (
+        <ul className="listStyleNone">
+          {data.publications.map((item: Publication) => (
+            <li key={data.publications.indexOf(item)} className="pointer pb-4">
+              <a href={item.link} target="_blank" className="text-reset text-decoration-none" rel="noreferrer">
+                <h4 className="fw-bold">{item.title}</h4>
+                <p>
+                  {item.authors}, {item.publishingCompany}, {item.city}&nbsp;
+                  {item.year}
+                </p>
+              </a>
+            </li>
+          ))}
+        </ul>
+      );
   };
 
   return (
